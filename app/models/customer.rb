3f18f1e32834e6d9c9199d69391e5a7f9e636459
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-24 17:03:41 -0500

    Added validations to Customer model, permit_params to ActiveAdmin.register Customer, and added assets/images/ to .gitignore

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
+  validates_presence_of :full_name
+
+  validates :phone_number, length: { minimum: 10, maximum: 10 },
+                           numericality: { only_integer: true }
 end
