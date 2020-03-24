28041cc50a7a9f9c3ce0518825f1b59a62ee54ff
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-24 18:06:13 -0500

    Added Bulma styling, modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get 'customers/index', as: 'customers'
 
-  get 'customers/:id', to: 'customers#show'
+  get 'customers/:id', to: 'customers#show', as: 'customer'
 end
