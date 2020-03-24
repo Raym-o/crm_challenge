d15cae8c95fe3bd0df7b6713824223986acdded6
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-24 17:48:23 -0500

    Added customer view _customer, index and show. Added routes for these views. Added controller actions to send to new customer routes and views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
+  # GET /
+  def index
+    @customers = Customer.all
+  end
+
+  # GET /customers/:id
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'customers/index', as: 'customers'
+
+  get 'customers/:id', to: 'customers#show'
 end
