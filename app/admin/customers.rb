c2be79eff2678401066d840144d150192e8887c6
Raymond Weiss <raymondaweiss@gmail.com>
2020-03-24 19:03:53 -0500

    Modified  ActiveAdmin.register Customers and Customer model to user ActiveStorage to attach image

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Customer do
-  permit_params :full_name, :phone_number, :email, :image, :notes
+  permit_params :full_name, :phone_number, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  has_one_attached :image
+
   validates_presence_of :full_name
 
   validates :phone_number, length: { minimum: 10, maximum: 10 },
